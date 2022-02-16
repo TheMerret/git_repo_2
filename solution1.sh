a8b78955638f85e6a2de0457446a70efea8e61a1
TheMerret <merret@bk.ru>
2022-02-16 12:11:13 +0300

    added scripts for generation

---
+++ solution1.sh
@@ -0,0 +1,10 @@
+git checkout -b branch1234567
+echo "1" > numbers.txt
+echo "2" >> numbers.txt
+echo "3" >> numbers.txt
+echo "4" >> numbers.txt
+echo "5" >> numbers.txt
+echo "6" >> numbers.txt
+echo "7" >> numbers.txt
+git commit -am "Новые цифры"
+git checkout master
---
+++ solution2.sh
@@ -0,0 +1,9 @@
+git branch branch4567890
+git checkout branch4567890
+echo "4" > numbers.txt
+echo "5" >> numbers.txt
+echo "6" >> numbers.txt
+echo "7" >> numbers.txt
+echo "8" >> numbers.txt
+echo "9" >> numbers.txt
+echo "0" >> numbers.txt
